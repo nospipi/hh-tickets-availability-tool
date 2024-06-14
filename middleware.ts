d71dce21017938ce2e019124e6b4f350a3e62984
Vaggelis Magonezos <nospipian@gmail.com>
2024-06-14 17:07:05 +0300

    Update middleware.ts

--- middleware.ts
+++ middleware.ts
@@ -1,20 +1,43 @@
 import { NextRequest, NextResponse } from "next/server"
 
-export function middleware(req: NextRequest) {
-  // Extract IP address and geo data
-  const ip = req.ip
-  const { nextUrl: url, geo } = req
+// export function middleware(req: NextRequest) {
+//   // Extract IP address and geo data
+//   const ip = req.ip
+//   const { nextUrl: url, geo } = req
 
+//   console.log("url:", url)
+//   console.log("geo:", geo)
+
+//   // Add the IP and geo data to the request headers (optional)
+//   const response = NextResponse.next()
+
+//   response.headers.set("X-Client-IP", ip || "UNKNOWN")
+//   response.headers.set("X-Client-Geo", JSON.stringify(geo) || "UNKNOWN")
+
+//   return response
+// }
+
+export async function middleware(req: NextRequest) {
+  const { nextUrl: url, geo } = req
   console.log("url:", url)
   console.log("geo:", geo)
 
-  // Add the IP and geo data to the request headers (optional)
-  const response = NextResponse.next()
+  const country = geo?.country || "UNKNOWN"
+  const city = geo?.city || "UNKNOWN"
+  const region = geo?.region || "UNKNOWN"
 
-  response.headers.set("X-Client-IP", ip || "UNKNOWN")
-  response.headers.set("X-Client-Geo", JSON.stringify(geo) || "UNKNOWN")
+  url.searchParams.set("country", country)
+  url.searchParams.set("city", city)
+  url.searchParams.set("region", region)
 
-  return response
+  //set headers for all
+  return NextResponse.rewrite(url, {
+    headers: {
+      "x-country": country,
+      "x-city": city,
+      "x-region": region,
+    },
+  })
 }
 
 export const config = {
