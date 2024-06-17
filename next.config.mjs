9df8b9d3da5a908b8f309be68ad7f4e5412eb2ba
Vaggelis Magonezos <nospipian@gmail.com>
2024-06-17 15:57:27 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    esmExternals: "loose",
+    serverComponentsExternalPackages: ["mongoose"],
+  },
   reactStrictMode: true,
   // other configurations
-};
+  webpack: (config) => {
+    config.experiments = {
+      topLevelAwait: true,
+    }
+    return config
+  },
+}
 
-export default nextConfig;
+export default nextConfig
